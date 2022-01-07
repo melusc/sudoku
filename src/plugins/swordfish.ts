fadafd33422c6ed0fddf8b8d9f9f38532410a659
Luca <lusc.dev@gmail.com>
2022-01-07 10:59:51 +0100

    Use maps

--- src/plugins/swordfish.ts
+++ src/plugins/swordfish.ts
@@ -6,9 +6,7 @@ type GetterName = 'getRow' | 'getCol';
 /* https://www.sudokuonline.io/tips/sudoku-swordfish-strategy */
 
 const swordfishByType = (sudoku: Sudoku, getterName: GetterName): boolean => {
-	debugger;
-
-	const indexedCells: Record<number, Record<number, number>> = {};
+	const indexedCells = new Map<number, Map<string, number>>();
 
 	/*
 		Index all cell possibles
@@ -18,29 +16,40 @@ const swordfishByType = (sudoku: Sudoku, getterName: GetterName): boolean => {
 	*/
 	for (let i = 0; i < 9; ++i) {
 		const structure = sudoku[getterName](i);
-		const currentIndex: Record<number, number> = {};
-		indexedCells[i] = currentIndex;
+		const currentIndex = new Map<string, number>();
+		indexedCells.set(i, currentIndex);
 
 		for (const [index, {possible}] of structure.entries()) {
 			for (const number of possible) {
-				currentIndex[Number(number)] ??= 0;
-				currentIndex[Number(number)] |= 2 ** index;
+				currentIndex.set(
+					number,
+					(currentIndex.get(number) ?? 0) | (2 ** index),
+				);
 			}
 		}
 	}
 
-	const merged: Record<number, Array<[key: number, indices: number[]]>> = {};
+	const merged = new Map<string, Array<[key: number, indices: number[]]>>();
 
-	for (const [index, indexed] of Object.entries(indexedCells)) {
-		for (const [number, key] of Object.entries(indexed)) {
-			merged[Number(number)] ??= [];
-			const array = merged[Number(number)]!;
+	for (const [index, indexed] of indexedCells) {
+		for (const [number, key] of indexed) {
+			// A completely empty sudoku will be a 9x9 swordfish
+			// but that is not helpful since nothing can be removed
+			if (bitCount(key) === 9) {
+				continue;
+			}
+
+			if (!merged.has(number)) {
+				merged.set(number, []);
+			}
+
+			const array = merged.get(number)!;
 
 			for (let i = 0, l = array.length; i < l; ++i) {
 				const item = array[i]!;
 
 				if ((item[0] & key) === key) {
-					item[1].push(Number(index));
+					item[1].push(index);
 				} else {
 					array.push([item[0] | key, [...item[1], Number(index)]]);
 				}
@@ -51,7 +60,7 @@ const swordfishByType = (sudoku: Sudoku, getterName: GetterName): boolean => {
 	}
 
 	let anyChanged = false;
-	for (const [number, indexed] of Object.entries(merged)) {
+	for (const [number, indexed] of merged) {
 		for (const [key, indices] of indexed) {
 			if (bitCount(key) !== indices.length) {
 				continue;
