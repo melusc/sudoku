599c0d7c8883f45cc3ea187c59c5ce8232cbe080
Luca <lusc.dev@gmail.com>
2022-01-15 17:11:23 +0100

    Add two swordfish tests

--- test/plugins/swordfish.test.ts
+++ test/plugins/swordfish.test.ts
@@ -1,12 +1,25 @@
-import test from 'ava';
-import {FixedLengthArray} from 'type-fest';
+import test, {ExecutionContext} from 'ava';
 import {swordfish} from '../../src/plugins/swordfish.js';
 import {Sudoku} from '../../src/sudoku.js';
 
-test('regular swordfish', t => {
-	const s = new Sudoku();
+const makeCheck
+	= (s: Sudoku, t: ExecutionContext) =>
+	(index: number, expected: number[] | number): void => {
+		const cell = s.getCell(index);
 
-	const layout: FixedLengthArray<FixedLengthArray<number | number[], 9>, 9> = [
+		if (Array.isArray(expected)) {
+			t.deepEqual(s.getCell(index).possible, new Set(expected));
+		} else {
+			t.is(cell.content, expected);
+			t.is(cell.possible.size, 0);
+		}
+	};
+
+test('regular swordfish #1', t => {
+	const s = new Sudoku();
+	const check = makeCheck(s, t);
+
+	const layout: Array<Array<number | number[]>> = [
 		[4, [1, 6], 2, [1, 7], 5, 3, 8, 9, [1, 7, 6]],
 		[[1, 7], 8, 3, 4, 9, 6, [1, 7], 2, 5],
 		// ^                     ^
@@ -35,17 +48,6 @@ test('regular swordfish', t => {
 	swordfish(s);
 	t.true(s.anyChanged);
 
-	const check = (index: number, expected: number[]): void => {
-		const cell = s.getCell(index);
-
-		if (expected.length === 1) {
-			t.is(cell.content, expected[0]!);
-			t.is(cell.possible.size, 0);
-		} else {
-			t.deepEqual(s.getCell(index).possible, new Set(expected));
-		}
-	};
-
 	check(1 * 9 + 0, [1, 7]); // was 1, 7
 	check(2 * 9 + 0, [6, 7]); // was 1, 6, 7
 	check(3 * 9 + 0, [3, 6]); // was 1, 3, 6
@@ -59,10 +61,111 @@ test('regular swordfish', t => {
 	check(4 * 9 + 6, [1, 3]); // was 1, 7
 });
 
+test('regular swordfish #2', t => {
+	const s = new Sudoku();
+	const check = makeCheck(s, t);
+
+	const layout: Array<Array<number | number[]>> = [
+		[[5, 6], [3, 5, 6], 2, [3, 4], 8, 1, [4, 5], 9, 7],
+		[7, [3, 5], 1, 6, 9, [3, 4], 2, [4, 5], 8],
+		[9, 8, 4, 5, 2, 7, 6, 3, 1],
+		[4, 2, 5, 1, 3, 8, 9, 7, 6],
+		[3, 7, 6, 9, 5, 2, 1, 8, 4],
+		[8, 1, 9, 7, 4, 6, 3, [2, 5], [2, 5]],
+		[[1, 6], [6, 9], 3, 8, 7, [4, 5], [4, 5], [1, 2], [2, 9]],
+		[2, [4, 5], 8, [3, 4], 1, 9, 7, 6, [3, 5]],
+		[[1, 5], [4, 5, 9], 7, 2, 6, [3, 4, 5], 8, [1, 4, 5], [3, 5, 9]],
+	];
+
+	for (const [rowIndex, row] of layout.entries()) {
+		const sRow = s.getRow(rowIndex);
+		for (const [colIndex, cell] of row.entries()) {
+			if (Array.isArray(cell)) {
+				sRow[colIndex]!.possible = new Set(cell);
+			} else {
+				sRow[colIndex]!.setContent(cell);
+			}
+		}
+	}
+
+	swordfish(s);
+	t.true(s.anyChanged);
+
+	check(0 * 9 + 1, [3, 6]);
+
+	check(1 * 9 + 1, [3, 5]);
+	check(1 * 9 + 7, [4, 5]);
+	check(1 * 9 + 8, 8);
+
+	check(5 * 9 + 1, 1);
+	check(5 * 9 + 7, [2, 5]);
+	check(5 * 9 + 8, [2, 5]);
+
+	check(7 * 9 + 1, [4, 5]);
+	check(7 * 9 + 7, 6);
+	check(7 * 9 + 8, [3, 5]);
+
+	check(8 * 9 + 1, [4, 9]);
+	check(8 * 9 + 7, [1, 4]);
+	check(8 * 9 + 8, [3, 9]);
+});
+
+test('Swordfish with little intersecting', t => {
+	const s = new Sudoku();
+	const check = makeCheck(s, t);
+
+	const layout: Array<Array<number | number[]>> = [
+		[[1], 3, 3, [1], 3, 3, 3, 3, 3],
+		// ^         ^
+		[3, [1], [1], 3, 3, 3, 3, 3, 3],
+		//   ^    ^
+		[[1], 3, [1], 3, 3, 3, 3, 3, 3],
+		// ^      ^
+		[3, [1], 3, [1], 3, 3, 3, 3, 3],
+		//   ^       ^
+		[[1, 2], [1, 2], [1, 2], [1, 2], 3, 3, 3, [1, 2], 3],
+		// ^      ^       ^       ^ remove all     ^ leave this
+		...Array.from({length: 4}, () => Array.from({length: 9}, () => 3)),
+	];
+
+	for (const [rowIndex, row] of layout.entries()) {
+		const sRow = s.getRow(rowIndex);
+		for (const [colIndex, cell] of row.entries()) {
+			if (Array.isArray(cell)) {
+				sRow[colIndex]!.possible = new Set(cell);
+			} else {
+				sRow[colIndex]!.setContent(cell);
+			}
+		}
+	}
+
+	swordfish(s);
+	t.true(s.anyChanged);
+
+	check(0 * 9 + 0, [1]);
+	check(0 * 9 + 3, [1]);
+
+	check(1 * 9 + 1, [1]);
+	check(1 * 9 + 2, [1]);
+
+	check(2 * 9 + 0, [1]);
+	check(2 * 9 + 2, [1]);
+
+	check(3 * 9 + 1, [1]);
+	check(3 * 9 + 3, [1]);
+
+	check(4 * 9 + 0, 2);
+	check(4 * 9 + 1, 2);
+	check(4 * 9 + 2, 2);
+	check(4 * 9 + 3, 2);
+	check(4 * 9 + 7, [1, 2]);
+});
+
 test('finned swordfish', t => {
 	const s = new Sudoku();
+	const check = makeCheck(s, t);
 
-	const layout: FixedLengthArray<FixedLengthArray<number | number[], 9>, 9> = [
+	const layout: Array<Array<number | number[]>> = [
 		[1, [3, 7, 8], [3, 6, 7, 8], 5, [3, 6, 8], 4, 9, 2, [6, 7]],
 		[[3, 8], 5, [3, 6, 7, 8], [2, 3, 6, 8], 9, [2, 3, 6, 8], 4, [6, 7, 8], 1],
 		[4, 9, 2, [1, 6, 8], 7, [1, 6, 8], 3, [6, 8], 5],
@@ -88,17 +191,6 @@ test('finned swordfish', t => {
 	swordfish(s);
 	t.true(s.anyChanged);
 
-	const check = (index: number, expected: number[]): void => {
-		const cell = s.getCell(index);
-
-		if (expected.length === 1) {
-			t.is(cell.content, expected[0]!);
-			t.is(cell.possible.size, 0);
-		} else {
-			t.deepEqual(s.getCell(index).possible, new Set(expected));
-		}
-	};
-
 	check(1 * 9 + 2, [3, 6, 7, 8]);
 	check(1 * 9 + 7, [6, 7, 8]);
 
@@ -106,7 +198,7 @@ test('finned swordfish', t => {
 	check(3 * 9 + 4, [1, 2, 7]);
 	check(3 * 9 + 5, [1, 2, 7]);
 
-	check(5 * 9 + 5, [1]);
+	check(5 * 9 + 5, 1);
 
 	check(6 * 9 + 5, [5, 7]);
 	check(6 * 9 + 7, [5, 7]);
